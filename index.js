f6832d016e24faa3e37d6e869ad53f49fba71381
catani-maximiliano <Catanimaximiliano@gmail.com>
2022-09-01 18:03:07 -0300

    entrega coderhouse clase 4

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-let number1 = prompt("ingrese un numero");
-number1 = parseInt(number1);
+let number1 = parseInt(prompt("ingrese un numero"));
+let number2 = parseInt(prompt("ingrese el segundo numero"));
 
-//el ParseInt() es para transformar de string a numero.
-let operacion = prompt(
-  "ingrese: suma(+), resta(-), division(/) o multiplicacion(*)"
-);
+let operacion = prompt("ingrese: suma(+), resta(-), division(/) o multiplicacion(*)");
 
-let number2 = prompt("ingrese el segundo numero");
-number2 = parseInt(number2);
 
-if (isNaN(number1) && isNaN(number2)) {
+//el setInterval(funcion, tiempo en milisegundos) sirve para realizar una funcion en un lapso de tiempo. lo utilizo para reiniciar la pagina .
+// location.reload() sirve para reiniciar la pagina.
 //isNaN() es una funcion donde devuelve false si es un numero y true si no lo es.
 
+function calculadora(number1, number2, operacion){
+if (isNaN(number1) && isNaN(number2)) {
   alert("no son numeros, por favor ingrese un numero");
   setInterval("location.reload()", 4000);
-//el setInterval(funcion, tiempo en milisegundos) sirve para realizar una funcion en un lapso de tiempo
-// location.reload() sirve para reiniciar la pagina.
-
 } else {
-  if (operacion == "suma") {
+  if (operacion == "+") {
     let suma = number1 + number2;
     alert("la suma es = " + suma);
     setInterval("location.reload()", 4000);
-  } else if (operacion == "resta") {
+  } else if (operacion == "-") {
     let resta = number1 - number2;
-    alert("la suma es = " + resta);
+    alert("la resta es = " + resta);
     setInterval("location.reload()", 4000);
-  } else if (operacion == "division") {
+  } else if (operacion == "/") {
     if (number2 !== 0) {
       let division = number1 / number2;
-      alert("la suma es = " + division);
+      alert("la division es = " + division);
       setInterval("location.reload()", 4000);
     } else {
       alert("no se puede dividir por cero");
       setInterval("location.reload()", 4000);
     }
-  } else if (operacion == "multiplicacion") {
+  } else if (operacion == "*") {
     let multiplicacion = number1 * number2;
-    alert("la suma es = " + multiplicacion);
+    alert("la multiplicacion es = " + multiplicacion);
     setInterval("location.reload()", 4000);
   } else {
     alert(
@@ -45,4 +40,7 @@ if (isNaN(number1) && isNaN(number2)) {
     );
     setInterval("location.reload()", 4000);
   }
+ }
 }
+
+calculadora(number1, number2, operacion);
